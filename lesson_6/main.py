930c8b200e8572e759e60ce028dc811d318ed57b
YamasipovDima_nout <d-ip@mail.ru>
2023-10-08 21:23:39 +0500

    reshenie 1 zadanie

--- lesson_6/main.py
+++ lesson_6/main.py
@@ -1,8 +1,10 @@
 gradus_out = int(input('Сколько градусов: '))
 gogo = 0
-while gradus_out <= 15:
+while gradus_out >= 15:
     gogo += 20
     gradus_out -= 2
-    break: gradus_out <= 15
-print('Растояние, ' gogo)
+    if gradus_out <= 15:
+        break
+    gogo += 10
+print(gogo)
 print('Забег окончен')
