1b0260830f5fe7fd7df0cf7b2066190603c5f59e
YamasipovDima_nout <d-ip@mail.ru>
2023-10-08 19:44:21 +0500

    main.py 1 trainer

---
+++ lesson_6/main.py
@@ -0,0 +1,8 @@
+gradus_out = int(input('Сколько градусов: '))
+gogo = 0
+while gradus_out <= 15:
+    gogo += 20
+    gradus_out -= 2
+    break: gradus_out <= 15
+print('Растояние, ' gogo)
+print('Забег окончен')
